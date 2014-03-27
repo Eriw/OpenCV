78b9285dedf388dbc00fc0533add15fba2485b61
Eriw <wirebrand@gmail.com>
2014-03-27 16:03:42 +0100

    * Erosion example

--- Basic/basic.cpp
+++ Basic/basic.cpp
@@ -1,26 +1,93 @@
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
+Mat loadImg(String str){
+	
+	Mat mat = imread(str);
+
+	return mat;
+}
+
+void createWindow(){
+
+	namedWindow("MyWindow", CV_WINDOW_AUTOSIZE); //create a window with the name "MyWindow"
+	namedWindow("Settings", CV_WINDOW_AUTOSIZE); //create a window with the name "MyWindow"
+
+}
+
+void showImage(Mat img){
+	imshow("MyWindow", img); //display the image which is stored in the 'img' in the "MyWindow" window
+}
+
+
+/// Global variables
+Mat src, erosion_dst, dilation_dst;
+
+int erosion_elem = 0;
+int erosion_size = 0;
+int dilation_elem = 0;
+int dilation_size = 0;
+int const max_elem = 2;
+int const max_kernel_size = 21;
+/**  @function Erosion  */
+void Erosion( int, void* )
+{
+  int erosion_type;
+  if( erosion_elem == 0 ){ erosion_type = MORPH_RECT; }
+  else if( erosion_elem == 1 ){ erosion_type = MORPH_CROSS; }
+  else if( erosion_elem == 2) { erosion_type = MORPH_ELLIPSE; }
+
+  Mat element = getStructuringElement( erosion_type,
+                                       Size( 2*erosion_size + 1, 2*erosion_size+1 ),
+                                       Point( erosion_size, erosion_size ) );
+
+  /// Apply the erosion operation
+  erode( src, erosion_dst, element );
+  imshow( "MyWindow", erosion_dst );
+}
+
+void initErosion(){
+	/// Create Erosion Trackbar
+	createTrackbar( "Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Settings",
+				&erosion_elem, max_elem,
+				Erosion );
+
+	createTrackbar( "Kernel size:\n 2n +1", "Settings",
+				&erosion_size, max_kernel_size,
+				Erosion );
+}
+
 int main( int argc, const char** argv )
 {
-     Mat img = imread("MyPic.JPG", CV_LOAD_IMAGE_UNCHANGED); //read the image data in the file "MyPic.JPG" and store it in 'img'
+	
+	Mat img = loadImg("img.jpg");
+	resize(img,img,Size(),2.0,2.0);
+	src = img;
 
-     if (img.empty()) //check whether the image is loaded or not
-     {
-          cout << "Error : Image cannot be loaded..!!" << endl;
-          //system("pause"); //wait for a key press
-          return -1;
-     }
+	createWindow();
 
-     namedWindow("MyWindow", CV_WINDOW_AUTOSIZE); //create a window with the name "MyWindow"
-     imshow("MyWindow", img); //display the image which is stored in the 'img' in the "MyWindow" window
+	initErosion();
 
-     waitKey(0); //wait infinite time for a keypress
+	showImage(img);
 
-     destroyWindow("MyWindow"); //destroy the window with the name, "MyWindow"
+	while(1){
+
+		//showImage(img);
+
+		
+
+		if( waitKey(30) == 27) //wait infinite time for a keypress
+			break;
+		
+	}
+
+	destroyWindow("MyWindow"); //destroy the window with the name, "MyWindow"
 
      return 0;
 }
+
+
